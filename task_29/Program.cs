e40844e337ac76d0ae9856655c4c5dd8e939db96
Katy <wc-katy@yandex.ru>
2022-08-15 22:10:51 +0700

    task 29

---
+++ task_29/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+// Сделала ручной ввод размера массива, ограничила случайные числа [0, 100)
+
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(99);
+    }
+    return result;
+}
+
+Console.Write("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Результат: [{String.Join("; ", GetArray(arraySize))}]");
