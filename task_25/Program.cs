92cd22e0a19ab12d4046ecb02c5b2fcc92c29304
Katy <wc-katy@yandex.ru>
2022-08-15 21:36:00 +0700

    task 25

---
+++ task_25/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную
+степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int Digree (int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
+
+Console.WriteLine($"Число {A} в степени {B} равно {Digree(A,B)}");
